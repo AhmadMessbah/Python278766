161e4c02919bb55052d82bed92bd84582f82cf9b
farzin-samsami <farzin_14@yahoo.com>
2023-08-29 16:14:03 +0330

    invoice

--- shop/model/entity/invoice.py
+++ shop/model/entity/invoice.py
@@ -1,2 +1,26 @@
-class Invoice:
-    pass
+import datetime
+
+from sqlalchemy import Column, Integer, String, ForeignKey
+from mft.model.entity.base import Base
+from sqlalchemy.orm import relationship
+
+
+class Invoice(Base):
+    __tablename__ = 'invoices'
+
+    code = Column(Integer, primary_key=True)
+    inv_code = Column(Integer)
+    custumer = Column(Integer)
+    custumer_id = Column(Integer, ForeignKey('customer.id'))
+    amount = Column(Integer)
+    discount = Column(Integer)
+    pure_price = Column(Integer)
+    status = Column(bool)
+    date_time = Column(datetime.datetime)
+    description = Column(string(50))
+
+
+    # customer = relationship("Customer", back_populates="invoices")
+
+    def __repr__(self):
+        return str(self.__dict__)
