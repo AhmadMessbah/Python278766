80d0eaceaa00308b218dad4396b110471b00cf7a
Darvish75 <md.darvish@gmail.com>
2023-09-03 15:02:50 +0330

    invoice_controller and invoice_item_controller added

---
+++ shop/controller/invoice_controller.py
@@ -0,0 +1,43 @@
+class InvoiceController:
+	
+	@classmethod
+	def save(cls):
+		try:
+			pass
+		except Exception as e:
+			pass
+
+	@classmethod
+	def edit(cls):
+		try:
+			pass
+		except Exception as e:
+			pass
+
+	@classmethod
+	def remove(cls):
+		try:
+			pass
+		except Exception as e:
+			pass
+
+	@classmethod
+	def find_all(cls):
+		try:
+			pass
+		except Exception as e:
+			pass
+
+	@classmethod
+	def find_by_inv_code(cls):
+		try:
+			pass
+		except Exception as e:
+			pass
+
+	@classmethod
+	def find_by_customer_id(cls):
+		try:
+			pass
+		except Exception as e:
+			pass
